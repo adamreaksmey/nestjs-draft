4c6fdc504ef69e41d6716065c77532af70bdb538
adamreaksmey <adaminiature@gmail.com>
2024-06-09 13:28:09 +0700

    added http exeception

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // HttpModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        ENVIRONMENT: Joi.string().required(),
+      }),
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
