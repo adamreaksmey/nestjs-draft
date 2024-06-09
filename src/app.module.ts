a8153617db9bd6fdd4a30850e489c6bab0730ba6
adamreaksmey <adaminiature@gmail.com>
2024-06-09 13:26:54 +0700

    added more necessary package

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
